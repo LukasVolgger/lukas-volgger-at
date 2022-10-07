45741d44149ddda71285eb32fc4b6d2c520eb127
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-10-07 09:01:34 +0200

    add fade-in animation to my-principles

--- src/app/my-principles/my-principles.component.ts
+++ src/app/my-principles/my-principles.component.ts
@@ -10,6 +10,21 @@ export class MyPrinciplesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.animatePrinciples();
+  }
+
+  /**
+   * Fades in the contents from left to right within a selected timeOut with an animation
+   */
+  animatePrinciples() {
+    let factor = 200;
+    for (let index = 1; index <= 4; index++) {
+      let timeOut = factor += 300;
+      setTimeout(() => {
+        const element = document.getElementById(`principle-${index}`);
+        element?.classList.add('fade-in-animation');
+      }, timeOut)
+    }
   }
 
 }
