0ebbcdb66b477d87bd2c796c8abdfbdf0d633b22
Lukas Volgger <lukas.volgger@live.at>
2022-11-08 13:54:01 +0100

    calculate and make my age variable

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
-
+  myBirthday: string = '1993-09-19';
+  myBirthdayTimestamp: number = new Date(this.myBirthday).getTime();
+  timestampNow: number = new Date().getTime();
+  myAgeInYears: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.myAgeInYears = Math.floor((this.timestampNow - this.myBirthdayTimestamp) / 31556926000);
   }
 
 }
