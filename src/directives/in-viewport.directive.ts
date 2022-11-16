ab8a04afbdd33b4aa42f97a913bb2abe99c3b3e8
Lukas Volgger <lukas.volgger@live.at>
2022-11-16 10:04:49 +0100

    fix nav scrollspy

--- src/directives/in-viewport.directive.ts
+++ src/directives/in-viewport.directive.ts
@@ -12,15 +12,13 @@ export class InViewportDirective {
 
   isScrolledIntoView() {
     const rect = this.element.nativeElement.getBoundingClientRect();
-    const topShow = rect.top <= window.innerHeight;
-    const bottomShow = rect.bottom <= window.innerHeight;
+    const topShow = rect.top <= window.innerHeight / 2;
+    const bottomShow = rect.bottom <= window.innerHeight / 2;
     const inView = topShow && bottomShow;
     if (inView) this.nav.currentInView = this.element.nativeElement.getAttribute('id');
 
 
-    // console.log('top:', topShow);
-    // console.log('bottom', bottomShow);
-    // console.log('current in view: ', this.nav.currentInView);
+    // console.log('current in view: ', this.nav.currentInView, 'top:', topShow, 'bottom', bottomShow);
 
   }
 
